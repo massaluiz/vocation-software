74d84ed6e78db4bce7a47c68920faaf7bd84ab93
Luiz Massa <luimassapontes@gmail.com>
2019-09-06 14:14:00 -0300

    This is the initial commit of project with one simple controller class (hello world) to test if api is working.

---
+++ src/main/java/com/company/vocationsoftware/VocationSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.company.vocationsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocationSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocationSoftwareApplication.class, args);
+	}
+
+}
